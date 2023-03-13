faeceaa1387a48784c71a19d394f60e9b228fea5
Rafi Shaik <rafishaik8208@gmail.com>
2023-03-13 18:07:08 +0000

    Commit message

---
+++ src/components/Home/index.js
@@ -0,0 +1,22 @@
+// Write your code here
+import Header from '../Header'
+
+const Home = () => (
+  <div className="home-container">
+    <Header />
+    <div className="home-image-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/home-sm-img.png"
+        alt="home"
+        className="home-small-image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+        alt="home"
+        className="home-large-image"
+      />
+    </div>
+  </div>
+)
+
+export default Home
